3f8bb5b4e05f1b7d3cf705717e580dc2bf3a2d07
nefarius <nefarius@dhmx.at>
2011-10-22 12:41:27 +0200

    Changed Test Application

---
+++ TrueTest/TestApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Dolinay;
+using System.IO;
+
+namespace TrueTest
+{
+    public partial class TestApp : Form
+    {
+        public TestApp()
+        {
+            InitializeComponent();
+        }
+
+        private void TestApp_Load(object sender, EventArgs e)
+        {
+            DriveDetector dd = new DriveDetector();
+            dd.DeviceArrived += new DriveDetectorEventHandler(dd_DeviceArrived);
+        }
+
+        void dd_DeviceArrived(object sender, DriveDetectorEventArgs e)
+        {
+            logBox.AppendText(e.Drive + Environment.NewLine);
+
+            string file = string.Format("{0}tm3identity", e.Drive);
+            using (StreamWriter sw = new StreamWriter(file))
+            {
+                sw.Write(System.Guid.NewGuid().ToString());
+            }
+            File.SetAttributes(file, File.GetAttributes(file) | FileAttributes.Hidden | FileAttributes.System);
+        }
+    }
+}
