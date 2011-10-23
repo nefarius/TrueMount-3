662df5398af02c5a776da7eceeaaaa4dd5014499
Benjamin Höglinger <nefarius@dhmx.at>
2011-10-23 12:59:00 +0200

    Added Program.cs

---
+++ TrueLib3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TrueLib
+{
+    public class Program
+    {
+        public string Executable { get; set; }
+        public string ArgumentLine { get; set; }
+        public bool HideWindow { get; set; }
+        public TimeSpan Delay { get; set; }
+        public bool Background { get; set; }
+
+        public Program()
+        {
+            HideWindow = false;
+            Delay = new TimeSpan(0, 0, 0);
+            Background = false;
+        }
+
+        public Program(string executable) : this() { this.Executable = executable; }
+
+        public void Launch()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = Executable;
+            p.StartInfo.Arguments = ArgumentLine;
+            p.StartInfo.WindowStyle = (HideWindow) ? ProcessWindowStyle.Hidden : ProcessWindowStyle.Normal;
+
+            Thread.Sleep(Delay);
+            p.Start();
+
+            if (!Background)
+                p.WaitForExit();
+        }
+    }
+}
